08215c734a33fd7ac25dbb7a9a44d48e7931d8d6
Mohammed Abid Zaheer <abidzaheer444@gmail.com>
2024-08-13 19:32:49 +0530

    added reponsiveness in contact  page

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -8,13 +8,13 @@ const Page = () => {
   return (
     <div>
       <TitleHead title={"Contact us"} desc={"Rabeh is a revolutionary fintech application that redefines the travel experience."} />
-      <section className="flex flex-col items-center gap-4">
+      <section className="flex flex-col items-center gap-4 p-5 md:p-0 ">
         <p className={"font-medium text-[#6941C6] " + fonts.spaceG.className}>About us</p>
-        <h1 className={"text-4xl font-bold " + fonts.urbanist.className}>We’re a distributed team</h1>
-        <h3 className={"text-xl text-[#475467] " + fonts.urbanist.className}>We have offices and teams all around the world.</h3>
+        <h1 className={"text-4xl font-bold text-center " + fonts.urbanist.className}>We’re a distributed team</h1>
+        <h3 className={"text-xl text-[#475467] text-center  " + fonts.urbanist.className}>We have offices and teams all around the world.</h3>
         <img src="/assets/map.png" alt="" className="w-full my-24 " />
       </section>
-      <section className={"grid grid-cols-12 my-4 " + fonts.inter.className}>
+      <section className={"grid md:grid-cols-12  my-4 " + fonts.inter.className}>
         <SingleCard title={"Support"} subtitle={"Our friendly team is here to help."} link={"support@rabeh.sa"} />
         <SingleCard title={"Sales"} subtitle={"Questions or queries? Get in touch!"} link={"Sales@rabeh.sa"} />
         <SingleCard title={"Phone"} subtitle={"Mon-Fri from 8am to 5pm."} link={"+1(202)-656-5556"} />
@@ -22,7 +22,7 @@ const Page = () => {
       <section className="my-24">
         <img src="/assets/map-2.png" alt="" className="w-full" />
       </section>
-      <section className="flex flex-col items-center gap-4 my-24">
+      <section className="flex flex-col items-center gap-4 my-24 p-5 md:p-0">
         <p className={"text-md text-primary font-bold " + fonts.spaceG.className}>Contact us</p>
         <h3 className={"text-4xl font-bold " + fonts.urbanist.className}>Get in touch</h3>
         <p className={"text-gray-500 " + fonts.urbanist.className}>We’d love to hear from you. Please fill out this form.</p>
