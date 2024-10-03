cb1cf80ea6346dc1a419c6920e76f4292d1d2e66
Syed Sayeeduddin <76648428+iamsayeeduddin@users.noreply.github.com>
2024-10-03 16:31:20 +0530

    Update BankInfo.jsx

--- src/app/[locale]/my-account/BankInfo.jsx
+++ src/app/[locale]/my-account/BankInfo.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useFormik } from "formik";
 import * as Yup from "yup";
 import useFonts from "@/utils/useFonts";
@@ -6,26 +6,25 @@ import { FaMapMarkerAlt, FaUpload } from "react-icons/fa";
 
 
 const validationSchema = Yup.object({
-  BankName: Yup.string().required("Bank Name is required"),
-  AccountHolderName: Yup.string().required("Account Holder Name is required"),
-  IBAN: Yup.string().required("IBAN is required").matches(
+  bankName: Yup.string().required("Bank Name is required"),
+  accountHolderName: Yup.string().required("Account Holder Name is required"),
+  iban: Yup.string().required("IBAN is required").matches(
     /^[A-Z]{2}\d{2}[A-Z0-9]{1,30}$/,
     "IBAN format is invalid"
-  ), // Adding regex for IBAN validation (customizable)
+  ),
 });
 
 
-const BankInfo = () => {
+const BankInfo = ({ data, handleUpdate, isLoading, isSuccess }) => {
   const fonts = useFonts();
   const [isEditing, setIsEditing] = useState(false);
 
   // Initial form values for Formik
   const initialValues = {
-    BankName: "Your Bank Name",
-    AccountHolderName: "Your Name",
-    IBAN: "Your IBAN",
+    bankName: "Your Bank Name",
+    accountHolderName: "Your Name",
+    iban: "Your IBAN",
   };
-  
 
   // Formik setup
   const formik = useFormik({
@@ -33,21 +32,43 @@ const BankInfo = () => {
     validationSchema,
     onSubmit: (values) => {
       // Handle save logic
-      console.log("User Info Saved:", values);
-      setIsEditing(false);
+      handleUpdate(values);
+      // setIsEditing(false);
     },
   });
+
+  const setInitVal = () => {
+    formik.setValues({
+      bankName: data?.bankName,
+      accountHolderName: data?.accountHolderName,
+      iban: data?.iban,
+    });
+  };
 
   const handleEditClick = () => {
     setIsEditing(true);
   };
 
+  useEffect(() => {
+    setInitVal();
+  }, [data]);
+
+  useEffect(() => {
+    if (isSuccess) {
+      setIsEditing(false);
+    }
+  }, [isLoading, isSuccess]);
+
+
   const handleCancel = () => {
+    setInitVal();
     setIsEditing(false);
   };
 
   return (
-    <div className={`personal-info ${fonts.spaceG.className} flex flex-col items-start justify-start`}>
+    <div
+      className={`personal-info ${fonts.spaceG.className} flex flex-col items-start justify-start`}
+    >
       <div className="w-full bg-white">
         <div className="flex flex-row justify-between">
           <h3 className="text-lg font-semibold text-center mb-6">
@@ -80,56 +101,89 @@ const BankInfo = () => {
           </div>
         </div>
 
-        <form onSubmit={formik.handleSubmit} className="user-info grid md:grid-cols-2 p-5">
+        <form
+          onSubmit={formik.handleSubmit}
+          className="user-info grid md:grid-cols-2 p-5"
+        >
           <div className="grid gap-4 mb-6">
             {isEditing ? (
-             <>
-             <div className="mb-6">
-           <label className="block tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="bankName">
-               Bank Name
-           </label>
-           <input
-               className={`appearance-none block w-full bg-white text-gray-700 border ${formik.touched.bankName && formik.errors.bankName ? "border-red-500" : "border-gray-200"} rounded-lg py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500`}
-               id="bankName"
-               type="text"
-               {...formik.getFieldProps("bankName")}
-           />
-           {formik.touched.bankName && formik.errors.bankName && (
-               <p className="text-red-500 text-xs italic">{formik.errors.bankName}</p>
-           )}
-       </div>
+              <>
+                <div className="mb-6">
+                  <label
+                    className="block tracking-wide text-gray-700 text-xs font-bold mb-2"
+                    htmlFor="bankName"
+                  >
+                    Bank Name
+                  </label>
+                  <input
+                    className={`appearance-none block w-full bg-white text-gray-700 border ${
+                      formik.touched.bankName && formik.errors.bankName
+                        ? "border-red-500"
+                        : "border-gray-200"
+                    } rounded-lg py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500`}
+                    id="bankName"
+                    type="text"
+                    {...formik.getFieldProps("bankName")}
+                  />
+                  {formik.touched.bankName && formik.errors.bankName && (
+                    <p className="text-red-500 text-xs italic">
+                      {formik.errors.bankName}
+                    </p>
+                  )}
+                </div>
 
-       <div className="mb-6">
-           <label className="block tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="accountHolderName">
-               Account Holder Name
-           </label>
-           <input
-               className={`appearance-none block w-full bg-white text-gray-700 border ${formik.touched.accountHolderName && formik.errors.accountHolderName ? "border-red-500" : "border-gray-200"} rounded-lg py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500`}
-               id="accountHolderName"
-               type="text"
-               {...formik.getFieldProps("accountHolderName")}
-           />
-           {formik.touched.accountHolderName && formik.errors.accountHolderName && (
-               <p className="text-red-500 text-xs italic">{formik.errors.accountHolderName}</p>
-           )}
-       </div>
+                <div className="mb-6">
+                  <label
+                    className="block tracking-wide text-gray-700 text-xs font-bold mb-2"
+                    htmlFor="accountHolderName"
+                  >
+                    Account Holder Name
+                  </label>
+                  <input
+                    className={`appearance-none block w-full bg-white text-gray-700 border ${
+                      formik.touched.accountHolderName &&
+                      formik.errors.accountHolderName
+                        ? "border-red-500"
+                        : "border-gray-200"
+                    } rounded-lg py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500`}
+                    id="accountHolderName"
+                    type="text"
+                    {...formik.getFieldProps("accountHolderName")}
+                  />
+                  {formik.touched.accountHolderName &&
+                    formik.errors.accountHolderName && (
+                      <p className="text-red-500 text-xs italic">
+                        {formik.errors.accountHolderName}
+                      </p>
+                    )}
+                </div>
 
-       <div className="mb-6">
-           <label className="block tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="iban">
-               IBAN
-           </label>
-           <input
-               className={`appearance-none block w-full bg-white text-gray-700 border ${formik.touched.iban && formik.errors.iban ? "border-red-500" : "border-gray-200"} rounded-lg py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500`}
-               id="iban"
-               type="text"
-               {...formik.getFieldProps("iban")}
-           />
-           {formik.touched.iban && formik.errors.iban && (
-               <p className="text-red-500 text-xs italic">{formik.errors.iban}</p>
-           )}
-       </div>
-       <div className="flex justify-between">
-  <button
+                <div className="mb-6">
+                  <label
+                    className="block tracking-wide text-gray-700 text-xs font-bold mb-2"
+                    htmlFor="iban"
+                  >
+                    IBAN
+                  </label>
+                  <input
+                    className={`appearance-none block w-full bg-white text-gray-700 border ${
+                      formik.touched.iban && formik.errors.iban
+                        ? "border-red-500"
+                        : "border-gray-200"
+                    } rounded-lg py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500`}
+                    id="iban"
+                    type="text"
+                    {...formik.getFieldProps("iban")}
+                  />
+                  {formik.touched.iban && formik.errors.iban && (
+                    <p className="text-red-500 text-xs italic">
+                      {formik.errors.iban}
+                    </p>
+                  )}
+                </div>
+                {JSON.stringify(formik.errors)}
+                <div className="flex justify-between">
+                  <button
                     type="button"
                     onClick={handleCancel}
                     className="  border border-[#CFD3DE] text-[#495162] font-bold py-2 px-4 rounded-lg w-full mr-2"
@@ -138,38 +192,43 @@ const BankInfo = () => {
                   </button>
                   <button
                     type="submit"
-                    className="bg-primary  text-white font-bold py-2 px-4 rounded-lg w-full"
+                    className={
+                      "bg-primary  text-white font-bold py-2 px-4 rounded-lg w-full " +
+                      (isLoading ? "animate-pulse" : "")
+                    }
                   >
-Save                  </button>
-
-            </div>
-
-            </>
+                    Save{" "}
+                  </button>
+                </div>
+              </>
             ) : (
               <>
-                  <>
+                <>
                   <div className="grid grid-rows-4 grid-flow-col  gap-5 mt-10">
-        <div>
-          <p className="text-[#495162] text-[12px] font-bold">Bank Name</p>
-          <p>{formik.values.BankName}</p>
-        </div>
+                    <div>
+                      <p className="text-[#495162] text-[12px] font-bold">
+                        Bank Name
+                      </p>
+                      <p>{formik.values.bankName || "NA"}</p>
+                    </div>
 
-        <div>
-          <p className="text-[#495162] text-[12px] font-bold">Account Holder Name</p>
-          <p>{formik.values.AccountHolderName}</p>
-        </div>
-        <div>
-          <p className="text-[#495162] text-[12px] font-bold">IBAN</p>
-          <p>{formik.values.IBAN}</p>
-        </div>
-        
-      </div>
-    </>
-                
+                    <div>
+                      <p className="text-[#495162] text-[12px] font-bold">
+                        Account Holder Name
+                      </p>
+                      <p>{formik.values.accountHolderName || "NA"}</p>
+                    </div>
+                    <div>
+                      <p className="text-[#495162] text-[12px] font-bold">
+                        IBAN
+                      </p>
+                      <p>{formik.values.iban || "NA"}</p>
+                    </div>
+                  </div>
+                </>
               </>
             )}
           </div>
-         
         </form>
       </div>
     </div>
